4d04d75270db7762413de561e8663eca3bb73528
AaronKanaron <65012247+AaronKanaron@users.noreply.github.com>
2021-08-30 22:38:38 +0200

    Hoem Comepeter

--- javascript/script.js
+++ javascript/script.js
@@ -2,47 +2,72 @@ const weekText = document.getElementById("weekText");
 const timeText = document.getElementById("timeText");
 const dateText = document.getElementById("dateText");
 const timeToEnd = document.getElementById("timeToEnd");
+const html = document.documentElement
+const currentdate = new Date(); 
 
-let now = new Date();
+let collectionID = 1041983
 
-//VECKA / 
-let startyear = new Date(now.getFullYear(), 0, 0);
-let diff = now - startyear;
-let oneWeek = 1000 * 60 * 60 * 24 * 7;
-let week = Math.floor(diff / oneWeek);
-let offset = 1
+/*
+TODO
+Fixa länk till unsplash bilden
+console.log(background)
 
-weekText.innerHTML = week + 1;
+Settings: CollectionId osv.
 
-
-function reloadTime(){
-    var currentdate = new Date(); 
-
-    var datetime = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear()  
-    var time = currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-    console.log(datetime)
-
-    document.title = time;
-    if(currentdate.getHours() >= 15){
-
-        timeToEnd.innerHTML = "Skolan har slutat!"
-    } else{
-        console.log(typeof currentdate.getDate())
-        timeToEnd.innerHTML = "Skolan slutar om:" + eval(15-currentdate.getHours() )
-
-    }
-    dateText.innerHTML = datetime
-    timeText.innerHTML = time;
+*/
+//Fetch Background
+function background(){
+	background = "https://source.unsplash.com/collection/" + collectionID + "/" + screen.width + "x" + screen.height
+	
+	html.style.backgroundImage = 'url("' + background + '")';
 }
 
-setInterval(() => {
-    reloadTime();
-}, 1000);
+//Klocka
+function reloadTime(){
+	const currentdate = new Date(); 
 
 
-//FUNCTIONS
+	let time = currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+	
+    document.title = time;
+    timeText.innerHTML = time;
+
+	setInterval(() =>{
+		
+		reloadTime();
+	}, 1000);
+}
+
+//Vecka
+function week() {
+
+	const startyear = new Date(currentdate.getFullYear(), 0, 0);
+	const diff = currentdate - startyear;
+	date
+	const oneWeek = 1000 * 60 * 60 * 24 * 7;
+	const week = Math.floor(diff / oneWeek);
+
+	weekText.innerHTML = "Vecka " + parseInt(week + 1);
+}
+
+//Datum
+function date() {
+	const days = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
+	const months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
+
+
+    let date = days[currentdate.getDay()].toLowerCase() + " " 
+				+ currentdate.getDate() + " "
+				+ months[currentdate.getMonth()].toLowerCase();
+
+	dateText.innerHTML = date;
+}
+
+
+//PageOnLoad Functions
 reloadTime();
+background();
+week();
+date();
